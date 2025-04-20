d9c8d7457a1d014983dc3974edbd9355ad0641da
coldweek3 <ohchanju3@naver.com>
2025-04-20 11:35:42 +0900

    update fontfamily

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 *{box-sizing:border-box}
 body, button, dd, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, input, legend, li, ol, p, select, table, td, textarea, th, ul {margin:0;padding:0}
-body, button, input, select, table, textarea {font-size:12px;line-height:16px;color:#202020;font-family:Uiyeun, BlinkMacSystemFont, "Malgun Gothic", "맑은 고딕", helvetica, "Apple SD Gothic Neo", sans-serif}
+body, button, input, select, table, textarea {font-size:12px;line-height:16px;color:#202020;font-family:Ownglyph_StudyHard-Rg, BlinkMacSystemFont, "Malgun Gothic", "맑은 고딕", helvetica, "Apple SD Gothic Neo", sans-serif}
 h1, h2, h3, h4, h5, h6 {font-size:inherit;line-height:inherit}
 textarea {-webkit-backface-visibility:hidden;backface-visibility:hidden;background-color:transparent;border:0;word-break:keep-all;word-wrap:break-word;outline:none;}
 button, input {-webkit-border-radius:0;border-radius:0;border:0}
